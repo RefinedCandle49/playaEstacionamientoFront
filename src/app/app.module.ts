d03844af674ab11fe7ae3f990e8a649ce0f400f4
DaikoDev <cdanieleon28@gmail.com>
2024-07-29 15:16:28 -0500

    feat: Declarar nuevos componentes en app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ParkingComponent } from './components/parking/parking.component';
+import { FormComponent } from './components/form/form.component';
+import { ParkingTableComponent } from './components/parking-table/parking-table.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SidebarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, SidebarComponent, ParkingComponent, FormComponent, ParkingTableComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
