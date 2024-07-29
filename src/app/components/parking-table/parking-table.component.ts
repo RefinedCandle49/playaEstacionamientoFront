f2e0afcea5e027849143eb6157e374f169d62ec1
DaikoDev <cdanieleon28@gmail.com>
2024-07-29 15:15:06 -0500

    build: Crear componente para tabla de estacionamiento

---
+++ src/app/components/parking-table/parking-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parking-table',
+  templateUrl: './parking-table.component.html',
+  styleUrl: './parking-table.component.css'
+})
+export class ParkingTableComponent {
+
+}
