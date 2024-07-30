98c451a0333e22767cca2b8e6535319510ba7283
DaikoDev <cdanieleon28@gmail.com>
2024-07-30 00:36:51 -0500

    feat: Declarar nuevos componentes en app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ParkingComponent } from './components/parking/parking.component';
 import { FormComponent } from './components/form/form.component';
 import { ParkingTableComponent } from './components/parking-table/parking-table.component';
+import { HistoryComponent } from './components/history/history.component';
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent, ParkingComponent, FormComponent, ParkingTableComponent],
+  declarations: [AppComponent, SidebarComponent, ParkingComponent, FormComponent, ParkingTableComponent, HistoryComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
