16d8ff987cc5746951366c1ea6b11166aaba2e93
DaikoDev <cdanieleon28@gmail.com>
2024-08-07 11:38:28 -0500

    feat: Crear funcionalidad para registrar nuevo ticket

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ParkingService } from '../../services/parking.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
 export class FormComponent {
+  parkingForm: FormGroup;
+  private parkingService = inject(ParkingService);
+  private fb = inject(FormBuilder);
 
+  // Constructor de los datos del formulario
+  constructor() {
+    this.parkingForm = this.fb.group({
+      name: '',
+      dni: '',
+      plate: '',
+      description: '',
+    });
+  }
+
+  // Enviar datos del formulario mediante un JSON
+  onSubmit() {
+    const formValues = this.parkingForm.value;
+
+    const parkingData = {
+      checkin: new Date().toLocaleString('sv-SE'),
+      checkout: null,
+      total: null,
+      cliente: {
+        name: formValues.name,
+        dni: formValues.dni,
+      },
+      vehicle: {
+        plate: formValues.plate,
+        description: formValues.description,
+      },
+    };
+
+    // Usar metodo POST del API
+    this.parkingService.create(parkingData).subscribe(
+      (response) => {
+        this.parkingForm.reset(); // Limpiar formulario
+
+        console.log('Vehiculo registrado: ', response);
+      },
+      (error) => {
+        console.error('Error al registrar el vehículo:', error);
+      }
+    );
+  }
 }
