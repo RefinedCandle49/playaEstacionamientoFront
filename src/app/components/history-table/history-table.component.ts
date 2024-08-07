c59c533a1c3082add13d893b0cfda84abd84db1b
DaikoDev <cdanieleon28@gmail.com>
2024-08-06 20:13:02 -0500

    feat: Agregar pipe para total

--- src/app/components/history-table/history-table.component.ts
+++ src/app/components/history-table/history-table.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ParkingService } from '../../services/parking.service';
 
 @Component({
   selector: 'app-history-table',
   templateUrl: './history-table.component.html',
-  styleUrl: './history-table.component.css'
+  styleUrl: './history-table.component.css',
 })
 export class HistoryTableComponent {
+  private parkingService = inject(ParkingService);
 
+  parking: any[] = [];
+
+  ngOnInit(): void {
+    this.parkingService.getFree().subscribe((parking: any) => {
+      this.parking = parking;
+    });
+  }
 }
