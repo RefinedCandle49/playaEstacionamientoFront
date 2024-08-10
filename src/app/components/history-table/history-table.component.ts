eff0e637020183b88e76c3cfaf2e37f204f71661
DaikoDev <cdanieleon28@gmail.com>
2024-08-10 17:44:07 -0500

    style: Ordenar tablas

--- src/app/components/history-table/history-table.component.ts
+++ src/app/components/history-table/history-table.component.ts
@@ -7,13 +7,25 @@ import { ParkingService } from '../../services/parking.service';
   styleUrl: './history-table.component.css',
 })
 export class HistoryTableComponent {
+  parking: any[] = [];
+  selectedParking: any;
   private parkingService = inject(ParkingService);
 
-  parking: any[] = [];
-
   ngOnInit(): void {
+    this.getParkingHistory();
+  }
+
+  // Obtener datos para tabla con total
+  getParkingHistory() {
     this.parkingService.getFree().subscribe((parking: any) => {
       this.parking = parking;
+    });
+  }
+
+  // Obtener datos por id
+  getDataForId(id: number) {
+    this.parkingService.getById(id).subscribe((parking: any) => {
+      this.selectedParking = parking;
     });
   }
 }
