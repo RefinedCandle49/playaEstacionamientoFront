52f624dc7b02ad66115df17889a90ce4a434d179
DaikoDev <cdanieleon28@gmail.com>
2024-08-21 19:11:28 -0500

    build: Intalar jsPDF

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import {
-  BrowserModule,
-  provideClientHydration,
+    BrowserModule,
+    provideClientHydration,
 } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,45 +15,45 @@ import { HistoryComponent } from './components/history/history.component';
 import { HistoryTableComponent } from './components/history-table/history-table.component';
 import { CommonModule, registerLocaleData } from '@angular/common';
 import localeEsPE from '@angular/common/locales/es-PE';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { VehicleTypePipe } from './pipes/vehicle-type.pipe';
 import { MainComponent } from './components/main/main.component';
-import {LoginComponent} from "./components/login/login.component";
+import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 registerLocaleData(localeEsPE, 'es-PE');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SidebarComponent,
-    ParkingComponent,
-    FormComponent,
-    ParkingTableComponent,
-    HistoryComponent,
-    HistoryTableComponent,
-    VehicleTypePipe,
-    MainComponent,
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    CommonModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot(),
-    FormsModule,
-  ],
-  providers: [
-    provideClientHydration(),
-    provideHttpClient(),
-    { provide: LOCALE_ID, useValue: 'es-PE' },
-  ],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        SidebarComponent,
+        ParkingComponent,
+        FormComponent,
+        ParkingTableComponent,
+        HistoryComponent,
+        HistoryTableComponent,
+        VehicleTypePipe,
+        MainComponent,
+        LoginComponent,
+        RegisterComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        CommonModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot(),
+        FormsModule,
+    ],
+    providers: [
+        provideClientHydration(),
+        provideHttpClient(),
+        { provide: LOCALE_ID, useValue: 'es-PE' },
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
